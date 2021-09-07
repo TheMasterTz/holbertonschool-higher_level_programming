1f12391b20f6547a00dc861d6bf467e774de9af4
Christian Diaz <84691244+TheMasterTz@users.noreply.github.com>
2021-09-07 18:11:13 -0500

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-number = 97
-while number <= 122:
-    print('{:c}'.format(number), end='')
-    number += 1
+for iter1 in range(97, 123):
+    print('{:c}'.format(iter1), end='')
