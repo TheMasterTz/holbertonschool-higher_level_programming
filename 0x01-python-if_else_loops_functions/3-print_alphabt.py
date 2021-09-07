0063efc482affe6aafa5fba428d89fa9d2c66bc1
Christian Diaz <84691244+TheMasterTz@users.noreply.github.com>
2021-09-07 18:14:01 -0500

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-number = 97
-while number <= 122:
+for number in range(97, 123):
     if (number != 101 and number != 113):
         print('{:c}'.format(number), end='')
-    number += 1
