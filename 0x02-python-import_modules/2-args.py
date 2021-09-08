bb2e2621e541b1596fec132d1d44d4e9a48a7d59
TheMasterTz <palavras2001@hotmail.com>
2021-09-08 13:26:59 -0500

    ejercicio 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    len_args = len(sys.argv)
+
+    if len_args > 1:
+        print(len_args, "arguments:")
+        for i in range(1, len_args + 1):
+            print('{}: {}'.format(i, sys.argv[i]))
+    elif len_args == 1:
+        print(len_args, "arguments:")
+        for i in range(1, len_args + 1):
+            print('{}: {}'.format(i, sys.argv[i]))
+    elif len_args == 0:
+        print(len_args, "arguments:")
