1c0a59fc0f28952ce0114f3ab5bfb773fd1c6862
TheMasterTz <palavras2001@hotmail.com>
2021-09-08 15:47:26 -0500

    ejercicio 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import hidden_4
-if __name__ == "__main__":
-    for i in dir(hidden_4):
-            if i[0] == '_' and i[1] == '_':
-                continue
-            print(i)
+
+if __name__ == '__main__':
+    names = dir(hidden_4)
+
+    for i in range(len(names)):
+        if names[i][:2] != '__':
+            print(names[i])
