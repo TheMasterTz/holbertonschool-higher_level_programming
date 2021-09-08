f415bf9783171a2424e18c9c4a817cec82be0416
TheMasterTz <palavras2001@hotmail.com>
2021-09-07 20:41:02 -0500

    ejercicio 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizzbuzz():
+    n = 1
+    while n < 101
+        if n % 3 == 0 and n % 5 == 0:
+            print('FizzBuzz', end=' ')
+        elif n % 3 == 0:
+            print('Fizz', end=' ')
+        elif n % 5 == 0:
+            print('Buzz', end=' ')
+        else:
+            print(n, end=' ')
+        n += 1
