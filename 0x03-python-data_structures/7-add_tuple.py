92464a5e5d5569c6340bdcd70fe08a6ae2f230ef
TheMasterTz <palavras2001@hotmail.com>
2021-09-13 18:02:47 -0500

    ejercicio 8

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,6 +4,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_b = validate_tuple(tuple_b)
     return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
 
+
 def validate_tuple(tuple_=()):
     if len(tuple_) < 2:
         if len(tuple_) == 1:
