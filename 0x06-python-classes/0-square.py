ae0096082c094efb5d73be73f2ddb465e61c91b2
Christian Diaz <84691244+TheMasterTz@users.noreply.github.com>
2021-09-28 16:37:50 -0500

    files for all

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Square class
+this class don't do anything for the moment
+"""
+class Square:
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class
+A Square Class
+"""
+class Square:
+    """__init__
+    The __init__ method initializes the size value
+    of the square.
+
+    Attributes:
+        size (int): The size of the square.
+    """
+    def __init__(self, size):
+        self.__size = size
