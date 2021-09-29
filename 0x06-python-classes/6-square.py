774237176400a1a9325a730e175cd428199a3f53
Christian Diaz <84691244+TheMasterTz@users.noreply.github.com>
2021-09-28 19:21:35 -0500

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -113,16 +113,3 @@ class Square:
 
             if j % self.__size == 0 and j > 0:
                 print()
-
-my_square_1 = Square(3)
-my_square_1.my_print()
-
-print("--")
-
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
-print("--")
-
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
