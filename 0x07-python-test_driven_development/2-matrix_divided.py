338d6017ed0149657bec5263e95140613b28cc90
Christian Diaz <84691244+TheMasterTz@users.noreply.github.com>
2021-10-07 17:04:38 -0500

    1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,42 +1,29 @@
-#!/usr/bin/python3
+##!/usr/bin/python3
 """
-Function that divides
-Return quotient in matrix
-
+This is the "2-matrix_divided" module.
+The 2-matrix_divided module supplies one function, matrix_divided(matrix, div).
 """
-
 
 def matrix_divided(matrix, div):
-    """
-    Divides all elements of a matrix
-    """
-    error1 = "matrix must be a matrix (list of lists) of integers/floats"
-    error2 = "Each row of the matrix must have the same size"
-    error3 = "div must be a number"
-
-    if not matrix:
-        raise TypeError(error1)
-
-    if matrix == [[]] or matrix == []:
-        raise TypeError(error1)
-
-    count = len(matrix[0])
-
+    """Divides all elements in the matrix by div"""
+    if type(matrix) is not list:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    length = None
     for i in matrix:
-        comp = len(i)
-        for j in i:
-            if type(j) is not int and type(j) is not float:
-                raise TypeError(error1)
-
-            if comp != count:
-                raise TypeError(error2)
-
+        if type(i) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        if length is None:
+            length = len(i)
+        elif length != len(i):
+            raise TypeError("Each row of the matrix must have the same size")
+        for n in i:
+            if type(n) is not int and type(n) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of \
+                integers/floats")
     if type(div) is not int and type(div) is not float:
-        raise TypeError(error3)
-
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
-    NewMatrix = [[round(t/div, 2) for t in row] for row in matrix]
-
-    return NewMatrix
+    return [[round(n / div, 2) for n in i] for i in matrix]
