af7a798e198fcea71fceffcf65098b0551e21048
Christian Diaz <84691244+TheMasterTz@users.noreply.github.com>
2021-10-13 10:19:27 -0500

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,12 +7,14 @@ from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
 
 NewList = []
 try:
-    NewList = load_from_json_file(filename)
+    NewList = load_from_json_file("add_item.json")
 except FileNotFoundError:
     NewList = []
 
-save_to_json_file(NewList + argv[1:], filename)
+for args in argv[1:]:
+    NewList.append(args)
+
+save_to_json_file(NewList , "add_item.json")
