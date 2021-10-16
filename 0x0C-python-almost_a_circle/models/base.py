060d92a0dab514dbb5b218550c4d4eb7c6ca01fb
Christian Diaz <84691244+TheMasterTz@users.noreply.github.com>
2021-10-16 11:00:18 -0500

    p

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -12,3 +12,21 @@ class Base:
             self.id = self.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        ListCart = []
+        if list_objs is not None:
+            for obj in list_objs:
+                ListCart.append(cls.to_json_string(obj))
+
+        with open(filename, 'w') as f:
+            f.write(cls.to_json_string(ListCart))
+
