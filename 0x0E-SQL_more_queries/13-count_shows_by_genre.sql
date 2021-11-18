51c538700d7e010b966c36c4a2b435a103ca45d7
TheMasterTz <palavras2001@hotmail.com>
2021-11-17 23:48:57 -0500

    all file

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- MySQL script
-SELECT tv_genres.title AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
