4b762cb21135a7f2e0b2346e7de3e7cc670709f9
TheMasterTz <palavras2001@hotmail.com>
2021-11-17 20:52:44 -0500

    all file

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,3 @@
 -- MySQL script
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON hbtn_0d_2.* TO user_0d_2@localhost;
-FLUSH PRIVILEGES;
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
