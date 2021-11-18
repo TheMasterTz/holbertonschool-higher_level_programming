c0c74e0ab227755d2795985571a6e09b0bc92717
TheMasterTz <palavras2001@hotmail.com>
2021-11-18 00:05:13 -0500

    all file

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,1 +1,9 @@
 -- MySQL script
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_genres
+ON tv_show_genres.show_id = tv_genres.id
+WHERE tv_shows.name = "Comedy"
+ORDER BY tv_shows.title;
