b8765bdf98e6dfd53876e5555f547ae10a675e97
TheMasterTz <palavras2001@hotmail.com>
2021-12-13 12:39:45 -0500

    all files

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+let args_length = process.argv.length;
+
+if (args_length === 2) {
+  console.log("No argument");
+} else if (args_length === 3) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
