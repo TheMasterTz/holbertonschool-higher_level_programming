a1997da1391c21254c44f659c67e506ac7aa6017
TheMasterTz <palavras2001@hotmail.com>
2021-12-13 15:18:51 -0500

    all files

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ const LISTS_NUMBER = process.argv.slice(2);
 if (LISTS_NUMBER.length > 1) {
   LISTS_NUMBER.sort();
   console.log(LISTS_NUMBER[LISTS_NUMBER.length - 2]);
+} else {
+  console.log(0);
 }
-
-console.log(0);
