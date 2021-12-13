9b4dec177cab86bd439e6382d5f508759456a1a3
TheMasterTz <palavras2001@hotmail.com>
2021-12-13 13:22:23 -0500

    all files

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const ARG_LENGTH = process.argv[2];
 
-if (ARGS_LENGTH === undefined) {
+if (ARG_LENGTH === undefined) {
   console.log('No argument');
 } else {
-  console.log(ARGS_LENGTH);
+  console.log(ARG_LENGTH);
 }
