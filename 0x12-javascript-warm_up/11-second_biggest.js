96533ef148633866274124c4bbea063061484658
TheMasterTz <palavras2001@hotmail.com>
2021-12-13 19:34:23 -0500

    all files

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
-
-if (process.argv.length <= 3) {
+const LIST = process.argv.slice(2).map(Number);
+if (LIST[0] === undefined) {
+  console.log(0);
+} else if (LIST.length === 1) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  const second = LIST.sort(function (a, b) {
+    return b - a;
+  })[1];
+  console.log(second);
 }
