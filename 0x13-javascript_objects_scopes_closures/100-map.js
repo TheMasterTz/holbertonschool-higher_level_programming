dbd8ed5d2f795366abfb4a6059c522767fff79c5
TheMasterTz <palavras2001@hotmail.com>
2021-12-14 13:04:15 -0500

    all file advanced

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,15 +1,7 @@
 #!/usr/bin/node
 
 const list = require('./100-data.js').list;
-const lis_temp = [0];
-
-function multiple_list (list) {
-  lis_temp = [0];
-  for (let iter = 0; list[iter + 1]; iter++) {
-    lis_temp.push(list[iter] * list[iter + 1]);
-  }
-  return lis_temp;
-}
+const lis_temp = list.map((x, index) => x * index);
 
 console.log(list);
-console.log(multiple_list(list));
+console.log(lis_temp);
