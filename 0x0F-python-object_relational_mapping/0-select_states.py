5e1baaa204993bc42c9cc5a2e7585b2a8bf3fdb8
TheMasterTz <palavras2001@hotmail.com>
2021-12-16 18:49:30 -0500

    all files

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+python script that lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    MySQL = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset='utf8'
+    )
+
+    cursor = MySQL.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    row = cursor.fetchall()
+    for state in row:
+        print(state)
+    cursor.close()
+    MySQL.close
