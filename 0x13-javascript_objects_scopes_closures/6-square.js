112954f8384a35c8f52770aeab7e23f8387704b2
TheMasterTz <palavras2001@hotmail.com>
2021-12-16 10:47:18 -0500

    Revert "files"
    This reverts commit 50fe78b521c4b6ee1e9879164fd5c708054673a5.

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,6 +2,14 @@
 const OldSquare = require('./5-square');
 
 module.exports = class Square extends OldSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  double () {
+    super.double();
+  }
+
   charPrint (c = 'X') {
     super.print(c);
   }
