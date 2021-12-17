279013a3e3b676bd26175bf7289a2bf40287883c
TheMasterTz <palavras2001@hotmail.com>
2021-12-17 10:24:05 -0500

    all files

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+python script that lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    MySQL = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset='utf8'
+    )
+
+    cursor = MySQL.cursor()
+    cursor.execute("""SELECT *
+                    FROM states
+                    WHERE name LIKE '{:s}'
+                    ORDER BY id ASC""".format(argv[4]))
+    row = cursor.fetchall()
+    for state in row:
+        if state[1] == argv[4]:
+            print(state)
+    cursor.close()
+    MySQL.close
