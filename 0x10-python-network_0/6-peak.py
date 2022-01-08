e1f2fc504054d13c367dae2c8df2e1e9b6e8db73
TheMasterTz <palavras2001@hotmail.com>
2022-01-08 18:56:16 -0500

    all file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,29 +5,25 @@ find peak of list of unsorted numbers
 
 
 def find_peak(list_of_integers):
-    """
-    find_peak list_of_integers
+    """"find_peak"""
 
-    Args:
-        list_of_integers (list): [description]
-
-    Returns:
-        int: [description]
-    """
     long = len(list_of_integers)
 
     if not list_of_integers:
         return None
 
-    for idx, i in enumerate(list_of_integers):
+    return function(list_of_integers, 0, long - 1)
 
-        if long <= 6:
-            if idx == 0:
-                idx = 1
-            if i > list_of_integers[idx - 1] and i > list_of_integers[idx + 1]:
-                return i
 
-        if i > list_of_integers[idx - 1] and i > list_of_integers[idx + 1]:
-                return i
+def function(list, start, end):
+    """"function"""
+    
+    mid = int((start + end) / 2)
 
-    return list_of_integers[0]
+    if mid + 1 >= len(list) or list[mid + 1] <= list[mid]:
+        if mid - 1 < 0 or list[mid - 1] <= list[mid]:
+            return list[mid]
+        else:
+            return function(list, 0, mid)
+    else:
+        return function(list, mid + 1, end)
