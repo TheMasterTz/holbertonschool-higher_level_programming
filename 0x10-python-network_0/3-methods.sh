7adcd91ce871d0759d833a609f4877ce70944ccd
TheMasterTz <palavras2001@hotmail.com>
2022-01-08 19:01:59 -0500

    all file

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-curl -sI "$1" | grep "Allow:" | cut -b 8-
+# Write a Bash script that takes in a URL and displays all HTTP methods the server will accept.
+curl -sI "$1" | grep Allow | cut -b 8-
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # header variable with curl
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s "$1" -H "X-School-User-Id: 98"
