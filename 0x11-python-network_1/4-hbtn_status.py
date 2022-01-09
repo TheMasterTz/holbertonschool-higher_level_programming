bd12820229eb253f1e15550964416f2b706d306d
TheMasterTz <palavras2001@hotmail.com>
2022-01-09 16:44:33 -0500

    all file

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ import requests
 if __name__ == "__main__":
     request = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(request)))
+    print("\t- type: {}".format(type(request.text)))
     print("\t- content: {}".format(request.text))
