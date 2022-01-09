9be997d36a025c3a4875883178a2c000f49dc336
TheMasterTz <palavras2001@hotmail.com>
2022-01-09 17:37:06 -0500

    all files

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ import requests
 
 if __name__ == '__main__':
     if len(argv[1]) == 0:
-        q=""
+        q = ""
     else:
         q = argv[1]
 
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     request = requests.post('http://0.0.0.0:5000/search_user', data)
     try:
         json = request.json()
-        if not json:
+        if not json or not json['id'] or not json['name']:
             print("No result")
         else:
             print("[{}] {}".format(json['id'], json['name']))
