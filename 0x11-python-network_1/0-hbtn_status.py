deee5e4b6fd464198e966a0280d4f8a8e8e8123c
TheMasterTz <palavras2001@hotmail.com>
2022-01-10 18:04:08 -0500

    add files

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,13 +2,14 @@
 """
 fetches https://intranet.hbtn.io/status
 """
-from urllib import request
 
-if __name__ == '__main__':
-    req = request.Request('https://intranet.hbtn.io/status')
-    with request.urlopen(req) as status:
-        html = status.read()
-        print("Body response:$")
-        print("\t- type: {}$".format(type(html)))
-        print("\t- content: {}$".format(html))
-        print("\t- utf8 content: {}$".format(html.decode('utf-8')))
+
+if __name__ == "__main__":
+    import urllib.request as request
+
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        readcontent = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(readcontent)))
+        print("\t- content: {}".format(readcontent))
+        print("\t- utf8 content: {}".format(readcontent.decode('utf8')))
