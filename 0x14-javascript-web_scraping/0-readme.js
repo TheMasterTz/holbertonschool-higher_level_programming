c3f53c255e31117dd98024803ceefeb7f57a29a7
TheMasterTz <palavras2001@hotmail.com>
2022-02-01 16:09:58 -0500

    all file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,7 +5,7 @@ const file = process.argv[2]
 fs.readFile(file, "utf8", (error, data) => {
   if (error) {
     console.error(error);
-  } else {
-    process.stdout.write(data);
+    return;
   }
+  console.log(data);
 });
