a9056cbe50f0886ed2f2ec5e09b11fee420cea62
TheMasterTz <palavras2001@hotmail.com>
2022-02-01 16:17:41 -0500

    all file

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 const args = process.argv.slice(2);
-const user_id = args[0];
-const url = 'https://swapi-api.hbtn.io/api/films/' + user_id;
+const userid = args[0];
+const url = 'https://swapi-api.hbtn.io/api/films/' + userid;
 
 request(url, function (error, response, body) {
   body = JSON.parse(body);
