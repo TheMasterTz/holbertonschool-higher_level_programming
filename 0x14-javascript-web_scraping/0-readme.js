3352075af32981d917d705bade2b828cdb2526bc
TheMasterTz <palavras2001@hotmail.com>
2022-02-01 16:08:24 -0500

    all file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const fs = require("fs");
+const file = process.argv[2]
 
-fs.readFile(process.argv[2], "utf8", (error, data) => {
+fs.readFile(file, "utf8", (error, data) => {
   if (error) {
     console.error(error);
   } else {
