9fb136b92217e3632e3325a6ccb973f7483614d7
TheMasterTz <palavras2001@hotmail.com>
2022-02-01 18:35:25 -0500

    all file

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,27 @@
 #!/usr/bin/node
 const request = require('request');
-const starWarsUri = 'https://swapi-api.hbtn.io/api/films/';
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films';
+let count = 0;
 
-request(starWarsUri, function (_err, _res, body) {
+request(starWarsUri, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode === 200) {
+    const json = JSON.parse(body).results;
 
+    for (let i = 0; i < json.length; i++) {
+      const characters = json[i].characters;
+
+      for (let j = 0; j < characters.length; j++) {
+        const character = characters[j];
+        const characterId = character.split('/');
+
+        if (characterId[5] == 18) {
+          count++;
+        }
+      }
+    }
+  }
+
+  console.log(count);
 });
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const request = require('request');
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/';
+
+request(starWarsUri, function (_err, _res, body) {
+
+});
