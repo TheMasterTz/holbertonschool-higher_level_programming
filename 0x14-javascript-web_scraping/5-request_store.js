319e0e0f6d59c56bdc57b2a8f61d87598c41a193
TheMasterTz <palavras2001@hotmail.com>
2022-02-01 19:35:24 -0500

    all file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,7 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
-const starWarsUri = 'https://swapi-api.hbtn.io/api/films/';
+const fs = require('fs');
+const url = process.argv[2];
+const filename = process.argv[3]
 
-request(starWarsUri, function (_err, _res, body) {
-
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode === 200) {
+    fs.writeFile(filename, body, 'utf8', (error) => {
+      if (error) {
+        console.error(error);
+      }
+    });
+  }
 });
