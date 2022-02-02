400f67e794c9ccfb8b649cf4ac4dd4e9548a088d
TheMasterTz <palavras2001@hotmail.com>
2022-02-01 23:59:32 -0500

    all file

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else {
-    const json = JSON.parse(body).characters;
-
-    for (let i = 0; i < json.length; i++) {
-      request(json[i], function (error, response, bodyjson) {
-        if (error) {
-          console.error(error);
-        }
-        console.log(JSON.parse(bodyjson).name);
-      });
-    }
+const argv = process.argv.slice(2);
+const url = 'https://swapi-api.hbtn.io/api/films/' + argv[0];
+request(url, function (err, response, body) {
+  if (!err) {
+    const characters = JSON.parse(body).characters;
+    helperChar(characters, 0);
   }
 });
+
+function helperChar (chars, i) {
+  request(chars[i], function (err, response, body) {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      if (i + 1 < chars.length) {
+        helperChar(chars, i + 1);
+      }
+    }
+  });
+}
