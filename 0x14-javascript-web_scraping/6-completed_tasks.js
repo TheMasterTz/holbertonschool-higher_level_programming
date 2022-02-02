a5dfd6cba1c2ce542833d6ac973728ee11cf08ee
TheMasterTz <palavras2001@hotmail.com>
2022-02-01 20:13:53 -0500

    all file

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,7 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
-const starWarsUri = 'https://swapi-api.hbtn.io/api/films/';
+const url = process.argv[2];
 
-request(starWarsUri, function (_err, _res, body) {
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode === 200) {
+    const tacksDisct = {}
+    const json = JSON.parse(body);
 
+    for (let i = 0; i < json.length; i++) {
+      const userId = json[i].userId;
+      const completed = json[i].completed;
+
+      if (!tacksDisct[userId]) {
+        tacksDisct[userId] = 0;
+      }
+      if (completed) ++tacksDisct[userId];
+    }
+    console.log(tacksDisct);
+  }
 });
