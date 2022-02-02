eea3999b5b624820f7dc9add99b7a4a31724e9c6
TheMasterTz <palavras2001@hotmail.com>
2022-02-01 19:36:23 -0500

    all file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const filename = process.argv[3]
+const filename = process.argv[3];
 
 request(url, function (error, response, body) {
   if (error) {
