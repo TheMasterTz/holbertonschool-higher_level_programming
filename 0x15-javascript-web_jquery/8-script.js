ef597dbd6bb3567a98f1373d8f7681b708eb9a04
TheMasterTz <palavras2001@hotmail.com>
2022-02-14 19:53:07 -0500

    all files

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -3,7 +3,7 @@ window.onload = function () {
   $.get("https://swapi-api.hbtn.io/api/films/?format=json", (data, status) => {
     if (status === "success") {
       for (let iter of data.results) {
-        $("#list_movies").append("<li>" + iter.title + "</li>");
+        $("#list_movies").append(`<li>${iter.title}</li>`);
       }
     }
   });
